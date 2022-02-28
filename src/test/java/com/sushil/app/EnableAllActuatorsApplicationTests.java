a571e338dd45bd05b4d6543beece12f861696ab3
Sushil Khera <sushilkhera@gmail.com>
2022-02-28 20:24:28 +0530

    Initial commit to demo Actuators Endpoints

---
+++ src/test/java/com/sushil/app/EnableAllActuatorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushil.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnableAllActuatorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
